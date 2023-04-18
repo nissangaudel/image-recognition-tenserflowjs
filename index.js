312a6dcb4fbf146fb7c8da1f594b6da17b689169
Nishan Gaudel <84219942+nissangaudel@users.noreply.github.com>
2023-04-17 23:48:34 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const img = document.getElementById('img');
+const button = document.getElementById('submit_button');
+const input = document.getElementById('image_url');
+const result = document.getElementById('prediction');
+result.style.visibility = 'hidden'
+
+        let model;
+
+        button.onclick = () => {
+            const url = input.value;
+            img.src = url;
+            result.style.visibility = 'visible'
+            result.innerText = "Loading...";
+        }
+
+        img.onload = () => {
+            doPrediction();
+        }
+
+        function doPrediction() {
+            if( model ) {
+                model.classify(img).then(predictions => {
+                    showPrediction(predictions);
+                });
+            } else {
+                mobilenet.load().then(_model => {
+                    model = _model;
+                    model.classify(img).then(predictions => {
+                        showPrediction(predictions);
+                    });
+                });
+            }
+        }
+
+        function showPrediction(predictions) {
+            result.innerText = "This might be a " + predictions[0].className;
+        }

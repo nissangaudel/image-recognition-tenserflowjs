24b0115004a2418497ce4a444ee9f7e44cdbffed
Nishan Gaudel <84219942+nissangaudel@users.noreply.github.com>
2023-04-22 10:11:44 -0400

    new

--- index.js
+++ index.js
@@ -1,80 +1,31 @@
 const img = document.getElementById('img');
-const button = document.getElementById('next');
+const button = document.getElementById('submit_button');
+const input = document.getElementById('image_url');
 const result = document.getElementById('prediction');
-result.style.display = 'none';
+result.style.visibility = 'hidden'
 
+        let model;
 
+        button.onclick = () => {
+            const url = input.value;
+            img.src = url;
+            result.style.visibility = 'visible'
+            result.innerText = "Loading...";
+        }
 
-const fileInput = document.getElementById('fileInput');
-const imageElement = document.getElementById('camera--output');
-const targetInput = document.getElementById('image_url');
-  
-        fileInput.addEventListener('change', function() {
-          const file = fileInput.files[0];
-          const reader = new FileReader();
-          reader.onload = function(e) {
-            imageElement.src = e.target.result;
-            targetInput.value = e.target.result;
-          }
-          reader.readAsDataURL(file);
-          result.style.display = 'block'
-          result.innerText = 'Loading...'
-          doPrediction();
-        });
-
-
-let model;
-
-
-button.onclick = () => {
-    cameraOutput.src = '//:0'
-}
-
-
-// Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };// Define constants
-const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")
-    
-// Access the device camera and stream to cameraView
-function cameraStart() {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function(stream) {
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
-    })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
-    });
-}
-
-// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
-    result.style.display = 'block'
-    result.innerText = 'Loading...'
-    doPrediction();
-};
-
-// Start the video stream when the window loads
-window.addEventListener("load", cameraStart, false);
+        img.onload = () => {
+            doPrediction();
+        }
 
         function doPrediction() {
             if( model ) {
-                model.classify(cameraOutput).then(predictions => {
+                model.classify(img).then(predictions => {
                     showPrediction(predictions);
                 });
             } else {
                 mobilenet.load().then(_model => {
                     model = _model;
-                    model.classify(cameraOutput).then(predictions => {
+                    model.classify(img).then(predictions => {
                         showPrediction(predictions);
                     });
                 });
